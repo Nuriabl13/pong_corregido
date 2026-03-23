c7c26eebf2ccb138ece1d5f2b4621af54d2f28a8
VVVi888 <vvvictor878@gmail.com>
2026-03-23 11:02:02 +0100

    Creada clase Jugador

---
+++ src/main/java/hlanz/programacion/pong/clases/Jugador.java
@@ -0,0 +1,27 @@
+package hlanz.programacion.pong.clases;
+
+import java.awt.*;
+
+public abstract class Jugador {
+    private int puntos;
+    private Raqueta raqueta;
+
+    public Jugador(Color c, int x){
+        this.puntos=0;
+        this.raqueta=new Raqueta(c,x);
+    }
+
+    public int getPuntos(){
+        return this.puntos;
+    }
+
+    public void sumarPunto(){
+        this.puntos++;
+    }
+
+    public Raqueta getRaqueta(){
+        return this.raqueta;
+    }
+
+    public abstract void realizarMovimiento();
+}
