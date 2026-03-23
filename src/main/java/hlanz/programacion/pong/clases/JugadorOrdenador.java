fbaefb63a627017b8114e0a9c4cf220f4c984def
VVVi888 <vvvictor878@gmail.com>
2026-03-23 11:07:01 +0100

    Creada clase JugadorOrdenador

---
+++ src/main/java/hlanz/programacion/pong/clases/JugadorOrdenador.java
@@ -0,0 +1,16 @@
+package hlanz.programacion.pong.clases;
+
+import java.awt.*;
+
+public abstract class JugadorOrdenador extends Jugador {
+    private Bola bola;
+
+    public JugadorOrdenador(Color c, int x, Bola b){
+        super(c,x);
+        this.bola=b;
+    }
+
+    public Bola getBola(){
+        return this.bola;
+    }
+}
